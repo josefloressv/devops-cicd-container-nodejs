87ab67fe2b8b490553cb86103fc984fa146db371
José Flores <josefloressv@gmail.com>
2022-03-17 01:55:09 -0600

    adding date and logs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => {  
+  console.log(getTimestampLog() + " procesando peticion...");
+  res.send("Hola Comunidad!");
+});
 
 app.get("/health", (req, res) => {
   res.status(200);
-  res.send("healthy");
+  res.send("UP");
 });
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
 });
+
+function getTimestampLog() {
+  return new Date((dt = new Date()).getTime() - dt.getTimezoneOffset() * 60000).toISOString().replace(/(.*)T(.*)\..*/,'$1 $2');
+}
